7012992dd247a46a2f202d53e1ffa04b3f286e12
Janne Bragge <jannebragger@kamk.fi>
2024-12-06 14:21:37 +0200

    päivitetty tiedostot

--- pseudo_dataset.R
+++
@@ -1,8 +0,0 @@
-# Tämä on uusi R-tiedosto
-set.seed(123)
-pseudo_data <- data.frame(
-  ID = 1:100,
-  Age = sample(18:65, 100, replace = TRUE),
-  Income = round(rnorm(100, mean = 50000, sd = 10000), 0),
-  Gender = sample(c("Male", "Female"), 100, replace = TRUE)
-)
